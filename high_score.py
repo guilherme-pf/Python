73521302965de7807ad49f040910b1f8ef890f85
guilherme-pf <110261175+guilherme-pf@users.noreply.github.com>
2022-08-22 00:24:25 -0300

    update

--- high_score.py
+++ high_score.py
@@ -7,4 +7,4 @@ max_score = student_scores[0]
 for i in range(len(student_scores)):
     if student_scores[i] > max_score:
         max_score = student_scores[i]
-print(max_score)
+print(f"The highest score in the class is: {max_score}")
