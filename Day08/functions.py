b0c93fdc7bffdaaed4c23aeb140769456860eb30
guilherme-pf <110261175+guilherme-pf@users.noreply.github.com>
2022-09-06 23:40:22 -0300

    funcrions

---
+++ Day08/functions.py
@@ -0,0 +1,17 @@
+def greet1():
+    print("Hello")
+
+greet1()
+
+
+def greet2(name):
+    print(f"Hello {name}")
+
+greet2("Gui")
+
+
+def greet3(name, location):
+    print(f"Hello {name}")
+    print(f"What is like in {location}?")
+
+greet3("Gui", "Piraciaba")
