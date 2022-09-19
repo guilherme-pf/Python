4153a1244fe239b5c9c781c53112917d2aaf6708
guilherme-pf <110261175+guilherme-pf@users.noreply.github.com>
2022-09-19 01:20:10 -0300

    update day_month with docstring

--- Day10/days_month.py
+++ Day10/days_month.py
@@ -11,6 +11,7 @@ def is_leap(year):
     return False
 
 def days_in_month(year, month):
+    """Return the number of days of any given year/month""" #Docstring to explain the function
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     if month > 12 or month < 1:
         return "Invalid month entered."
@@ -25,5 +26,6 @@ def days_in_month(year, month):
 #🚨 Do NOT change any of the code below 
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
-days = days_in_month(year, month)
+days = days_in_month(year,month)
 print(days)
+
